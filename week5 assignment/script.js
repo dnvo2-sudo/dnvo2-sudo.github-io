f61eea11e9dea08b47b44a2ef9f16db367292cc5
dnvo2-sudo <dnvo2@asu.edu>
2025-11-16 13:44:51 -0700

    week5 update

--- week5 assignment/script.js
+++ week5 assignment/script.js
@@ -4,12 +4,19 @@ document.getElementById("menu").addEventListener("click", hideBar);
 
 function hideBar() {
     let sideBarHideShow = document.getElementsByClassName('navStyle')[0];
+    let content = document.getElementsByClassName('container-content')[0];
 
     if (isOpen) {
         sideBarHideShow.style.display = "none";
+
+        content.style.left = "13vw";
+        content.style.width = "80%";
         isOpen = false;
     } else {
         sideBarHideShow.style.display = "block";
+
+        content.style.left = "25vw";
+        content.style.width = "65%";
         isOpen = true;
     }
 }
